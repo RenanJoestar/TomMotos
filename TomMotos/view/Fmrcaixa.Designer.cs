475ee75ac682e3a02b13623d7ac0e991d04277d7
samucazikka007 <81575418+samucazikka007@users.noreply.github.com>
2021-10-22 15:11:08 -0300

    Caixa
    Parte de serviço criada agora só falta o CRUD

--- TomMotos/view/Fmrcaixa.Designer.cs
+++ TomMotos/view/Fmrcaixa.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ltb_nota = new System.Windows.Forms.ListBox();
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
             this.txtsubitotal = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,35 +44,39 @@
             this.txtPrU = new System.Windows.Forms.TextBox();
             this.txtPt = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDescServ = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_pmo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAc = new System.Windows.Forms.Button();
             this.rb_produto = new System.Windows.Forms.RadioButton();
             this.rb_servico = new System.Windows.Forms.RadioButton();
-            this.label7 = new System.Windows.Forms.Label();
-            this.lb_descricao = new System.Windows.Forms.ListBox();
             this.ptb_imagem = new System.Windows.Forms.PictureBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnIv = new System.Windows.Forms.Button();
-            this.btnAc = new System.Windows.Forms.Button();
+            this.BtnExcluir_item = new System.Windows.Forms.Button();
+            this.listView_venda = new System.Windows.Forms.ListView();
+            this.ch_item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_desc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_qtd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_vU = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch_vI = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptb_imagem)).BeginInit();
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // ltb_nota
-            // 
-            this.ltb_nota.FormattingEnabled = true;
-            this.ltb_nota.Location = new System.Drawing.Point(699, 5);
-            this.ltb_nota.Name = "ltb_nota";
-            this.ltb_nota.Size = new System.Drawing.Size(410, 186);
-            this.ltb_nota.TabIndex = 0;
-            this.ltb_nota.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
             // txtsubitotal
             // 
-            this.txtsubitotal.Location = new System.Drawing.Point(887, 239);
+            this.txtsubitotal.Location = new System.Drawing.Point(1090, 576);
             this.txtsubitotal.Name = "txtsubitotal";
             this.txtsubitotal.Size = new System.Drawing.Size(151, 20);
             this.txtsubitotal.TabIndex = 1;
@@ -78,7 +84,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(835, 242);
+            this.label1.Location = new System.Drawing.Point(1015, 579);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 2;
@@ -159,23 +165,67 @@
             // 
             // txtPt
             // 
-            this.txtPt.Location = new System.Drawing.Point(35, 188);
+            this.txtPt.Location = new System.Drawing.Point(35, 184);
             this.txtPt.Name = "txtPt";
             this.txtPt.Size = new System.Drawing.Size(243, 20);
             this.txtPt.TabIndex = 13;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtDescServ);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txt_pmo);
             this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.lb_descricao);
             this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Location = new System.Drawing.Point(315, 266);
+            this.groupBox1.Location = new System.Drawing.Point(849, 16);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(321, 146);
+            this.groupBox1.Size = new System.Drawing.Size(345, 239);
             this.groupBox1.TabIndex = 14;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Serviço";
+            // 
+            // txtDescServ
+            // 
+            this.txtDescServ.Location = new System.Drawing.Point(24, 32);
+            this.txtDescServ.Name = "txtDescServ";
+            this.txtDescServ.Size = new System.Drawing.Size(299, 104);
+            this.txtDescServ.TabIndex = 21;
+            this.txtDescServ.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // txt_pmo
+            // 
+            this.txt_pmo.Location = new System.Drawing.Point(24, 155);
+            this.txt_pmo.Name = "txt_pmo";
+            this.txt_pmo.Size = new System.Drawing.Size(254, 20);
+            this.txt_pmo.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(140, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "PREÇO DA MÃO DE OBRA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(138, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "DESCRIÇÃO DO SERVIÇO";
             // 
             // groupBox2
             // 
@@ -197,6 +247,16 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Produto";
             // 
+            // btnAc
+            // 
+            this.btnAc.Location = new System.Drawing.Point(115, 210);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(75, 23);
+            this.btnAc.TabIndex = 20;
+            this.btnAc.Text = "Acrescentar";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.button2_Click);
+            // 
             // rb_produto
             // 
             this.rb_produto.AutoSize = true;
@@ -211,30 +271,13 @@
             // rb_servico
             // 
             this.rb_servico.AutoSize = true;
-            this.rb_servico.Location = new System.Drawing.Point(281, 266);
+            this.rb_servico.Location = new System.Drawing.Point(815, 16);
             this.rb_servico.Name = "rb_servico";
             this.rb_servico.Size = new System.Drawing.Size(28, 17);
             this.rb_servico.TabIndex = 17;
             this.rb_servico.TabStop = true;
             this.rb_servico.Text = " ";
             this.rb_servico.UseVisualStyleBackColor = true;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(21, 16);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(138, 13);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "DESCRIÇÃO DO SERVIÇO";
-            // 
-            // lb_descricao
-            // 
-            this.lb_descricao.FormattingEnabled = true;
-            this.lb_descricao.Location = new System.Drawing.Point(24, 32);
-            this.lb_descricao.Name = "lb_descricao";
-            this.lb_descricao.Size = new System.Drawing.Size(279, 56);
-            this.lb_descricao.TabIndex = 7;
             // 
             // ptb_imagem
             // 
@@ -256,25 +299,9 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "IMAGEM";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(23, 91);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(140, 13);
-            this.label8.TabIndex = 9;
-            this.label8.Text = "PREÇO DA MÃO DE OBRA";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(24, 107);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(254, 20);
-            this.textBox1.TabIndex = 13;
-            // 
             // btnIv
             // 
-            this.btnIv.Location = new System.Drawing.Point(657, 237);
+            this.btnIv.Location = new System.Drawing.Point(49, 232);
             this.btnIv.Name = "btnIv";
             this.btnIv.Size = new System.Drawing.Size(116, 23);
             this.btnIv.TabIndex = 19;
@@ -282,21 +309,114 @@
             this.btnIv.UseVisualStyleBackColor = true;
             this.btnIv.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnAc
+            // BtnExcluir_item
             // 
-            this.btnAc.Location = new System.Drawing.Point(115, 210);
-            this.btnAc.Name = "btnAc";
-            this.btnAc.Size = new System.Drawing.Size(75, 23);
-            this.btnAc.TabIndex = 20;
-            this.btnAc.Text = "Acrescentar";
-            this.btnAc.UseVisualStyleBackColor = true;
-            this.btnAc.Click += new System.EventHandler(this.button2_Click);
+            this.BtnExcluir_item.Location = new System.Drawing.Point(504, 457);
+            this.BtnExcluir_item.Name = "BtnExcluir_item";
+            this.BtnExcluir_item.Size = new System.Drawing.Size(75, 23);
+            this.BtnExcluir_item.TabIndex = 20;
+            this.BtnExcluir_item.Text = "Excluir item";
+            this.BtnExcluir_item.UseVisualStyleBackColor = true;
+            this.BtnExcluir_item.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listView_venda
+            // 
+            this.listView_venda.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_item,
+            this.ch_id,
+            this.ch_desc,
+            this.ch_qtd,
+            this.ch_vU,
+            this.ch_vI});
+            this.listView_venda.FullRowSelect = true;
+            this.listView_venda.GridLines = true;
+            this.listView_venda.HideSelection = false;
+            this.listView_venda.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.listView_venda.Location = new System.Drawing.Point(315, 272);
+            this.listView_venda.Name = "listView_venda";
+            this.listView_venda.Size = new System.Drawing.Size(505, 179);
+            this.listView_venda.TabIndex = 21;
+            this.listView_venda.UseCompatibleStateImageBehavior = false;
+            this.listView_venda.View = System.Windows.Forms.View.Details;
+            this.listView_venda.SelectedIndexChanged += new System.EventHandler(this.listView_venda_SelectedIndexChanged);
+            // 
+            // ch_item
+            // 
+            this.ch_item.Text = "ITEM";
+            this.ch_item.Width = 40;
+            // 
+            // ch_id
+            // 
+            this.ch_id.Text = "CODIGO";
+            this.ch_id.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ch_desc
+            // 
+            this.ch_desc.Text = "DESCRIÇÃO";
+            this.ch_desc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ch_desc.Width = 160;
+            // 
+            // ch_qtd
+            // 
+            this.ch_qtd.Text = "QTD";
+            this.ch_qtd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ch_vU
+            // 
+            this.ch_vU.Text = "VL.UNIT.(R$)";
+            this.ch_vU.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ch_vU.Width = 90;
+            // 
+            // ch_vI
+            // 
+            this.ch_vI.Text = "VL.ITEM.(R$)";
+            this.ch_vI.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ch_vI.Width = 90;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(849, 272);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(368, 179);
+            this.listView1.TabIndex = 22;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ITEM";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "DESCRIÇÃO";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 253;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "VALOR ";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 70;
             // 
             // Fmrcaixa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1278, 579);
+            this.ClientSize = new System.Drawing.Size(1547, 635);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listView_venda);
+            this.Controls.Add(this.BtnExcluir_item);
             this.Controls.Add(this.btnIv);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.rb_servico);
@@ -305,7 +425,6 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtsubitotal);
-            this.Controls.Add(this.ltb_nota);
             this.Name = "Fmrcaixa";
             this.Text = "Fmrcaixa";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Fmrcaixa_FormClosed);
@@ -322,8 +441,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox ltb_nota;
         private System.Windows.Forms.TextBox txtsubitotal;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -338,15 +455,28 @@
         private System.Windows.Forms.TextBox txtPrU;
         private System.Windows.Forms.TextBox txtPt;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ListBox lb_descricao;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RadioButton rb_produto;
         private System.Windows.Forms.RadioButton rb_servico;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_pmo;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button btnIv;
         private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.Button BtnExcluir_item;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox txtDescServ;
+        private System.Windows.Forms.ListView listView_venda;
+        private System.Windows.Forms.ColumnHeader ch_item;
+        private System.Windows.Forms.ColumnHeader ch_id;
+        private System.Windows.Forms.ColumnHeader ch_desc;
+        private System.Windows.Forms.ColumnHeader ch_qtd;
+        private System.Windows.Forms.ColumnHeader ch_vU;
+        private System.Windows.Forms.ColumnHeader ch_vI;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
--- TomMotos/view/Fmrcaixa.cs
+++ TomMotos/view/Fmrcaixa.cs
@@ -5,8 +5,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TomMotos.Conexao;
@@ -15,7 +16,11 @@ namespace TomMotos.view
 {
     public partial class Fmrcaixa : Form
     {
-        string id_produto, nome_produto,valor_total;
+        string[] SERVICO = new string[4];
+        string[] item = new string[6];
+        List<string> nota = new List<string>();
+        string id_produto, nome_produto;
+        int valor_total, itens = 0, servicos = 0;
         MySqlConnection conexao = ConnectionFactory.getConnection();
         public Fmrcaixa()
         {
@@ -30,22 +35,62 @@ namespace TomMotos.view
 
         private void Fmrcaixa_FormClosed(object sender, FormClosedEventArgs e)
         {
+            
             Fmrsumario fmrsumario = new Fmrsumario();
             fmrsumario.Show();
         }
 
         private void Fmrcaixa_Load(object sender, EventArgs e)
         {
-            btnAc.Visible = false;
+            listView_venda.Items.Clear();
+            txtsubitotal.Text = 0.ToString();
+            // btnAc.Visible = false;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (listView_venda.Items.Count > 0)
+                listView_venda.Items.Remove(listView_venda.SelectedItems[0]);
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int preco_servico = 0, pt;
+            preco_servico = int.Parse(txt_pmo.Text);
+            pt = int.Parse(txtsubitotal.Text);
+            if (txt_pmo.Text != "" || txtDescServ.Text != "")
+            {
+
+                if (servicos == 0) servicos = 1;
+                else servicos += 1;
+
+                SERVICO[1] = txtDescServ.Text.ToString();
+                SERVICO[2] = txt_pmo.Text.ToString();
+
+                SERVICO[0] = servicos.ToString();
+                listView1.Items.Add(new ListViewItem(SERVICO));
+                pt += preco_servico;
+                txtsubitotal.Text = pt.ToString();
+            }
+            else MessageBox.Show("VERIFIQUE SE EXISTE CAMPOS EM BRANCOS");
+
+
+        }
+
+        private void txt_Ds_TextChanged(object sender, EventArgs e)
+        {
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            btnAc.Visible = true;
-            ltb_nota.Items.Add("--------------------------------------------------------------------------------------------------------------------------------------");
-            ltb_nota.Items.Add("ITEM Codigo                           Descricao                                                                                       ");
-            ltb_nota.Items.Add("QTD      UN                          VL.UNIT.(R$)                                   VL.ITEM(R$)");
-            ltb_nota.Items.Add("--------------------------------------------------------------------------------------------------------------------------------------");
+            btnAc.Visible = true;            
+
+        }
+
+        private void listView_venda_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -77,14 +122,31 @@ namespace TomMotos.view
 
                 total = total + (valor_produto * int.Parse(txtqtd.Text));
                 txtsubitotal.Text = total.ToString();
-                ltb_nota.Items.Add("" + id_produto + "                                               " + nome_produto + "                                                                                       ");
-                ltb_nota.Items.Add("    " + txtqtd.Text.ToString() + " x UN                                        R$ " + valor_produto + "                                                       R$  " + valor_produto * int.Parse(txtqtd.Text));
-
+                valor_total = valor_produto * int.Parse(txtqtd.Text);
+                if (itens == 0)
+                    itens = 1;
+                else itens += 1;
+                //if (string.IsNullOrEmpty(id_produto) || string.IsNullOrEmpty(nome_produto) || string.IsNullOrEmpty(valor_produto.ToString()) || string.IsNullOrEmpty(txtqtd.Text)) 
+                // return;                
+                item[0] = itens.ToString();
+                item[1] = id_produto;
+                item[2] = nome_produto;
+                item[3] = txtqtd.Text;
+                item[4] = valor_produto.ToString();
+                item[5] = valor_total.ToString();
+               
+                listView_venda.Items.Add(new ListViewItem(item));
+                //txtIdProduto.Clear();
+                //txtdesc.Clear();
+               // txtIdProduto.Focus();
+                
+                //listView_venda.//( "1", id_produto, nome_produto, txtqtd.Text, valor_produto, valor_total));
+                //ltb_nota.Items.Add("    " + txtqtd.Text.ToString() + " x UN                                        R$ " + valor_produto + "                                                       R$  " + valor_produto * int.Parse(txtqtd.Text));
+                //ltb_nota.DataSource = null;
+                //ltb_nota.DataSource = nota;
+                
             }
             else MessageBox.Show("PRODUTO NÃO ENCONTRADO!!");
-
-
-
         }
     }
 }
