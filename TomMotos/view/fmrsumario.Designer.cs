a6a94c3b4a8253257fe40abd9b9b7f74feba46ac
RenanJO <renan.leite.wow@gmail.com>
2021-11-29 15:29:54 -0300

    update comit

--- TomMotos/view/fmrsumario.Designer.cs
+++ TomMotos/view/fmrsumario.Designer.cs
@@ -46,7 +46,7 @@ namespace TomMotos
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelForm = new System.Windows.Forms.Panel();
             this.Panel = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -311,28 +311,28 @@ namespace TomMotos
             this.Panel.Size = new System.Drawing.Size(936, 566);
             this.Panel.TabIndex = 30;
             // 
-            // button1
+            // btnlimpar
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.btnlimpar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(62)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(1129, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 45);
-            this.button1.TabIndex = 31;
-            this.button1.Text = "X";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.btnlimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(41)))), ((int)(((byte)(62)))));
+            this.btnlimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlimpar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpar.ForeColor = System.Drawing.Color.White;
+            this.btnlimpar.Location = new System.Drawing.Point(1129, 23);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(45, 45);
+            this.btnlimpar.TabIndex = 31;
+            this.btnlimpar.Text = "X";
+            this.btnlimpar.UseVisualStyleBackColor = false;
+            this.btnlimpar.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Fmrsumario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1204, 703);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnlimpar);
             this.Controls.Add(this.Panel);
             this.Controls.Add(this.panel2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -361,7 +361,7 @@ namespace TomMotos
         private System.Windows.Forms.Button BT_PRODUTO;
         private System.Windows.Forms.Panel panelForm;
         private System.Windows.Forms.Panel Panel;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnlimpar;
         private System.Windows.Forms.Button btnVendas;
         private System.Windows.Forms.Button btnCaixa;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
